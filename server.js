dc4324bd5b9930ba1d02efd0365ae2178ae6ce97
Nathanyel Sandi <nathanyel.sandi@cobalt.io>
2022-07-03 23:02:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,11 +13,18 @@ const express = require('express');
 const app = express();
 app.use(middlewares)
 app.use(router)
-//app.use(requireHTTPS);
 app.use(express.static('./dist/projetocadastroveiculos'));
 
 app.get('/*', (req, res) =>
   res.sendFile('index.html', {root: 'dist/projetocadastroveiculos/'}),
 );
 
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+      return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+app.use(requireHTTPS);
 app.listen(8080);
