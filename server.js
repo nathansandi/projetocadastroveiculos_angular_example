7b644171b960e87513a3038def0e7ca505c20d33
Nathanyel Sandi <nathanyel.sandi@cobalt.io>
2022-07-03 18:24:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.use(express.static('./dist/projetocadastroveiculos'));
 
 
 
-const port = process.env.PORT || 3000
+const port = 3000
 
-server.listen(port, () => {
+server.listen(3000, () => {
     console.log(`JSON Server is running on port ${port}`)
 })
 
