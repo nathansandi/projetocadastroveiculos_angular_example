ad167625038f17fb56b76b1bf7516cd19063bef8
Nathanyel Sandi <nathanyel.sandi@cobalt.io>
2022-07-03 18:20:13 +0200

    Add json service

--- server.js
+++ server.js
@@ -9,8 +9,25 @@ function requireHTTPS(req, res, next) {
 const express = require('express');
 const app = express();
 
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('api/db.json')
+
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+
 app.use(requireHTTPS);
 app.use(express.static('./dist/projetocadastroveiculos'));
+
+
+
+const port = process.env.PORT || 3000
+
+server.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}`)
+})
 
 app.get('/*', (req, res) =>
   res.sendFile('index.html', {root: 'dist/projetocadastroveiculos/'}),
